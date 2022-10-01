29b6813315c68e0809ce0a07212b80657c7fab62
Nathan <wenjuncai97@gmail.com>
2022-10-01 02:25:41 -0400

    set up movie API

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 
 function Banner() {
+
+  const [movie, setMovie] = useState([]);
+
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+  }
+
   return (
     <header className='banner' style={{
         backgroundSize: "cover",
@@ -17,7 +24,7 @@ function Banner() {
         <button className="banner_button">Play</button>
         <button className="banner_button">My List</button>
       </div>
-      <h1 className="banner_description">This is a test description</h1>
+      <h1 className="banner_description">{truncate(`This is a test description`, 150)}</h1>
     </div>
 
     <div className="banner--fadeBottom" />
