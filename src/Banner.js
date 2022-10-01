26a12e888f4fea879a1a8fcf044a859de0cf4646
Nathan <wenjuncai97@gmail.com>
2022-10-01 02:37:17 -0400

    added movie name and description functionality

--- src/Banner.js
+++ src/Banner.js
@@ -1,9 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from './axios';
+import requests from './Requests'
 
 function Banner() {
 
   const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, [])
+
+  console.log(movie)
 
   function truncate(string, n) {
     return string?.length > n ? string.substr(0, n - 1) + '...' : string;
@@ -12,19 +29,19 @@ function Banner() {
   return (
     <header className='banner' style={{
         backgroundSize: "cover",
-        backgroundImage: `url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASIAAACuCAMAAAClZfCTAAAAA1BMVEUAAACnej3aAAAASElEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeA8XKAAFZcBBuAAAAAElFTkSuQmCC")`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: "center center",
     }}>
 
     <div className="banner_contents">
       <h1 className="banner_title">
-        Movie Name
+        {movie?.title || movie?.name || movie?.original_name}
       </h1>
       <div className="banner_buttons">
         <button className="banner_button">Play</button>
         <button className="banner_button">My List</button>
       </div>
-      <h1 className="banner_description">{truncate(`This is a test description`, 150)}</h1>
+      <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
     </div>
 
     <div className="banner--fadeBottom" />
