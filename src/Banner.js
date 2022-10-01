80b29a0ce895bbd6bdbc5c9e4053028d429f4c0c
Nathan <wenjuncai97@gmail.com>
2022-10-01 01:41:19 -0400

    added homescreen, nav

---
+++ src/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <header className='banner' style{{
+        backgroundImage: `url`
+    }}>
+    </header>
+  )
+}
+
+export default Banner
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDI8Nwx4V3KS7IbyvV3HGtwQWAN2IyOfZU",
+    authDomain: "netflix-nathan.firebaseapp.com",
+    projectId: "netflix-nathan",
+    storageBucket: "netflix-nathan.appspot.com",
+    messagingSenderId: "1041741037567",
+    appId: "1:1041741037567:web:03372af41251fb8058f1ca"
+  };
