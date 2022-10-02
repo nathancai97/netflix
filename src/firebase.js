b66b8830345f5f6b8d79e3e80440246fd1bfac61
Nathan <wenjuncai97@gmail.com>
2022-10-01 23:28:20 -0400

    initialized firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDI8Nwx4V3KS7IbyvV3HGtwQWAN2IyOfZU",
-    authDomain: "netflix-nathan.firebaseapp.com",
-    projectId: "netflix-nathan",
-    storageBucket: "netflix-nathan.appspot.com",
-    messagingSenderId: "1041741037567",
-    appId: "1:1041741037567:web:03372af41251fb8058f1ca"
-  };
+  apiKey: "AIzaSyAClk11KcjpHxrE7aVB_ljlHjGHxqdR_8o",
+  authDomain: "netflix-nathancai.firebaseapp.com",
+  projectId: "netflix-nathancai",
+  storageBucket: "netflix-nathancai.appspot.com",
+  messagingSenderId: "139171026065",
+  appId: "1:139171026065:web:070ff6d9dab758a361db30"
+};
