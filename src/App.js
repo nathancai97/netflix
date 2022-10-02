320271d139bbcda6f413152837ebb008c645cc19
Nathan <wenjuncai97@gmail.com>
2022-10-02 01:23:12 -0400

    user stays logged in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -7,9 +7,21 @@ import {
 import HomeScreen from './screens/HomeScreen';
 import './App.css';
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
 
 function App() {
+
   const user = null;
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if(userAuth) {
+        console.log(userAuth)
+      } else {
+
+      }
+    })
+  }, [])
 
   return (
     <div className="app">
