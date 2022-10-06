ac4da218131ba957bd5c66bb2ad519e61268c519
Nathan <wenjuncai97@gmail.com>
2022-10-05 20:48:29 -0400

    added plans screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { selectUser } from '../features/userSlice';
 import { auth } from '../firebase';
 import Nav from '../Nav';
+import PlansScreen from './PlansScreen';
 import './ProfileScreen.css';
 
 function ProfileScreen() {
@@ -20,6 +21,7 @@ function ProfileScreen() {
                     <h2>{user.email}</h2>
                     <div className="profileScreen_plans">
                         <h3>Plans</h3>
+                        <PlansScreen />
                         <button onClick={() => auth.signOut()} className="profileScreen_signOut">Sign Out</button>
                     </div>
                 </div>
